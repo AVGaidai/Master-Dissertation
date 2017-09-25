9fe79518203c34a40dd7185e1db8861f6f27b817
AVGaidai <diligent20494@gmail.com>
2017-09-25 17:49:59 +0700

    fixed MPI_Matrix_Partition

--- MPI_Gauss/main.c
+++ MPI_Gauss/main.c
@@ -40,9 +40,20 @@ void print_matrix(double *matrix, int SIZE_X, int SIZE_Y)
     }
 }
 
-
+/**
+ * \brief Partition of the input matrix into parts.
+ *
+ * \param file descriptor of the input matrix.
+ * \param pointer to the output part.
+ * \param pointer to the number of rows in the output part.
+ * \param pointer to the number of columns in the output part.
+ */
 void MPI_Matrix_Partition(FILE *fp, double **part, int *ROWS, int *COLUMNS)
 {
+   /*
+    * cnt is counter of rows in the output part
+    * row is currently processed row
+    */
     int rank, commsize, i, j, cnt, receiver;
     double *row;
 
@@ -54,72 +65,80 @@ void MPI_Matrix_Partition(FILE *fp, double **part, int *ROWS, int *COLUMNS)
     printf("Hello World, I'am %d process!\n", rank);
     *part = NULL;
     cnt = 0;
+    /* Main process */
     if (rank == 0) {
+        /* Reading matrix size */
         fscanf(fp, "%d %d", ROWS, COLUMNS);
+        /* Allocate memory for row */
         row = (double *) malloc(*COLUMNS * sizeof(double));
+        /* Alternate reading matrix rows */
         for (i = 0; i < *ROWS; ++i) {
             for (j = 0; j < *COLUMNS; ++j) {
                 fscanf(fp, "%lf", &row[j]);
             }
+            /* Define receiver */
             receiver = i % commsize;
+            /* For main process */
             if (receiver == 0) {
-                *part = (double *) realloc(*part, (cnt + 1) * sizeof(double));
+                /* Reallocate memory and add new row in the output part */
+                *part = (double *) realloc(*part, (cnt + 1) *
+                                           (*COLUMNS) * sizeof(double));
                 memcpy((void *) *part + cnt * (*COLUMNS),
                        (void *) row,
                        sizeof(double) * (*COLUMNS));  
-                ++cnt;
+                ++cnt; // Increase counter of the rows
+            /* For other rows */
             } else {
-                //printf("send to %d:\n", receiver);
-                //print_matrix(row, 1, *COLUMNS);
-                MPI_Send((const void *) row,
-                         *COLUMNS,
-                         MPI_DOUBLE,
-                         receiver,
-                         receiver,
-                         MPI_COMM_WORLD);
+                /* Send the row to receiver */
+                MPI_Send((const void *) row, // Row
+                         *COLUMNS,           // Number of columns in the row
+                         MPI_DOUBLE,         // Elements type
+                         receiver,           // Receiver
+                         receiver,           // Tag
+                         MPI_COMM_WORLD);    // Commutator
             }
         }
+        /* Sending message about the end reading to all process */ 
         for (i = 1; i < commsize; ++i) { 
-            MPI_Send((const void *) row,
-                     *COLUMNS,
-                     MPI_DOUBLE,
-                     i,
-                     0,
-                     MPI_COMM_WORLD);
+            MPI_Send((const void *) row, // Any row (dummy)
+                     *COLUMNS,           // Number of columns in the row
+                     MPI_DOUBLE,         // Elements type
+                     i,                  // Receiver
+                     0,                  // Tag
+                     MPI_COMM_WORLD);    // Commutator
         }
     }
-    
+
+    /* Broadcast message with nomber of columns */
     MPI_Bcast((void *) COLUMNS, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Barrier(MPI_COMM_WORLD);
 
+    /* For other process */
     if (rank != 0) {
-        //sleep(2);
+        /* Allocate memory for row */
         row = (double *) malloc(*COLUMNS * sizeof(double));
-        
+        /* Waiting for message */
         while (1) {
-            MPI_Recv((void *) row,
-                     *COLUMNS,
-                     MPI_DOUBLE,
-                     0,
-                     MPI_ANY_TAG,
-                     MPI_COMM_WORLD,
-                     &status);
+            /* Receive row of the matrix */
+            MPI_Recv((void *) row,   // Row
+                     *COLUMNS,       // Number of columns in the row
+                     MPI_DOUBLE,     // Elements type
+                     0,              // Sender
+                     MPI_ANY_TAG,    // Tag
+                     MPI_COMM_WORLD, // Commetator
+                     &status);       // Status
 
-             if (!status.MPI_TAG) break;
-            //sleep(rank);
-            //printf("%d\n", rank);
-            //print_matrix(row, 1, *COLUMNS);
-            *part = (double *) realloc(*part, (cnt + 1) * sizeof(double));
+            /* If receive message about the end reading */ 
+            if (!status.MPI_TAG) break;
+            /* Reallocate memory and add new row in the output part */
+            *part = (double *) realloc(*part, (cnt + 1) *
+                                       (*COLUMNS) * sizeof(double));
             memcpy((void *) *part + cnt * (*COLUMNS),
                    (void *) row,
                    sizeof(double) * (*COLUMNS));  
-            ++cnt;
+            ++cnt; // Increase counter of the rows
         }
     }
     *ROWS = cnt;
-    //sleep(rank);
-    //print_matrix(*part, *ROWS, *COLUMNS);
-    MPI_Barrier(MPI_COMM_WORLD);    
     free(row);
 }
 
@@ -182,7 +201,6 @@ int main(int argc, char *argv[])
         fp = fopen(argv[1], "r");
     
     MPI_Matrix_Partition(fp, &matrix, &ROWS, &COLUMNS);
-    //MPI_Barrier(MPI_COMM_WORLD);
 
     if (rank == 0)
         fclose(fp);
@@ -190,6 +208,7 @@ int main(int argc, char *argv[])
     sleep(rank);
     print_matrix(matrix, ROWS, COLUMNS);
 
-    //if (ROWS) free(matrix);
+    if (ROWS) free(matrix);
+
     MPI_Finalize();
 }
